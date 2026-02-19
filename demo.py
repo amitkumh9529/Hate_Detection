5b83a4132100216a684abf0a3ed4d3d2c440099f
Amit Halder <amith3169@gmail.com>
2026-02-19 23:23:05 +0530

    datasets

---
+++ demo.py
@@ -0,0 +1,12 @@
+from hate.logger import logging
+from hate.exception import CustomException
+import sys
+
+
+# logging.info("Hello World")
+
+try:
+    a = 1 / "0"
+
+except Exception as e:
+    raise CustomException(e, sys) from e     
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup (
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="amit",
+    author_email="amith3169@gmail.com",
+    packages=find_packages(),
+    install_requires = [],
+)
